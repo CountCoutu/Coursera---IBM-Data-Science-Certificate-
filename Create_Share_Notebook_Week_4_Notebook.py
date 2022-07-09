eaa88e0ea89649392281e1307e6dfa9678fbb48b
CountCoutu <108560095+CountCoutu@users.noreply.github.com>
2022-07-09 15:26:21 -0400

    Add files via upload

---
+++ Create_Share_Notebook_Week_4_Notebook.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Joshua Coutu**<br>
+# Data Scientist
+
+# *I am intersted in data science because I'd like to pivot my career to use my degrees in mathematics in a more challenging and high-growth field.*
+
+# ### In Cell 5, the code will create a function with the input n as the exponent "e" is raised to, and then return the result
+
+# In[2]:
+
+
+from math import e
+def exp(n):
+    return e**n
+# example
+
+exp(2)
+
+
+# **Creating a Table in Markdown**
+# 
+# 
+# x|f(x)=x+2|g(x)=x^2
+# ---|---|---
+# *n/a*|`linear`|**quadratic**
+# 0|2|0
+# 1|3|1
+# 2|4|4
+# 3|5|9
+# 
+# **Creating a bullet list:**
+# <br>Grocery List:
+# - apples
+# - peppers
+# - peanut butter
+# 
+# **Creating a numbered list:**
+# <br>Agenda:
+# 1. Warm Up
+# 2. Notes
+# 3. Practice Problems
+# 4. Quiz
